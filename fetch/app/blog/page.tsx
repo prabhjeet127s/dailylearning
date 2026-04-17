0dd16a922eecbaf950b10dfc0e19488befbcb65e
Drmac <drmac@Drmacs-MacBook-Air.local>
2026-04-17 10:39:03 +0530

    learning

---
+++ fetch/app/blog/page.tsx
@@ -0,0 +1,14 @@
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const posts = await res.json()
+
+  return (
+    <div>
+      <h1>Blog</h1>
+
+      {posts.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </div>
+  )
+}
